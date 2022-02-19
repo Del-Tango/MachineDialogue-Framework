669ab1d135110ba617cd3ed43e3a98714aea00fb
Del:Tango <alvearesolutions@gmail.com>
2022-02-18 20:35:47 -0500

    Updated the FlowCTRL automation framework.

--- src/FlowCTRL-Framework/src/backpack/bp_shell.py
+++ src/FlowCTRL-Framework/src/backpack/bp_shell.py
@@ -1,19 +1,21 @@
+#!/usr/bin/python3
+#
+# Regards, the Alveare Solutions #!/Society -x
+#
+# SHELL
+
 import logging
-#import pysnooper
 
 from subprocess import Popen, PIPE
 
-log = logging.getLogger('FlowCTRL')
-
-# SHELL CMD
+log = logging.getLogger('FloodGateNODE')
 
 
-#@pysnooper.snoop()
 def shell_cmd(command, user=None):
     log.debug('')
     log.debug('Issuing system command: ({})'.format(command))
     if user:
-        command = "su - -c \'{}\'".format(command)
+        command = "su {} -c \'{}\'".format(user, command)
     process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
     output, errors = process.communicate()
     log.debug('Output: ({}), Errors: ({})'.format(output, errors))
